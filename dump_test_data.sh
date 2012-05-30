25f5bf3906f09de6866600418ef27a1f7e0ef080
mschneid <mschneid@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-05-30 15:50:06 +0000

    MegDb: -file name change

--- dump_test_data.sh
+++ dump_test_data.sh
@@ -4,4 +4,4 @@ SRC_DB_PORT=5440
 SRC_DB_NAME=megdb_r8
 SCHEMA_LIST="-n core -n partitions -n cv -n elayers -n pgq -n pgq_ext -n web_r8 -n silva_r102_ssu -n silva_r102_lsu -n pfam_23 -n pfam_24"
 
-pg_dump -Fc --data-only -f dumpfile ${SCHEMA_LIST} -p ${SRC_DB_PORT} ${SRC_DB_NAME}
+pg_dump -Fc --data-only -f megdb.dump ${SCHEMA_LIST} -p ${SRC_DB_PORT} ${SRC_DB_NAME}
--- restore_test_data.sh
+++ restore_test_data.sh
@@ -5,5 +5,5 @@ SRC_DB_NAME=megdb
 SCHEMA_LIST="-n core -n partitions -n cv -n elayers -n pgq -n pgq_ext -n web_r8 -n silva_r102_ssu -n silva_r102_lsu -n pfam_23 -n pfam_24"
 
 psql -p ${SRC_DB_PORT} -f before_pg_restore_test_data.sql $SRC_DB_NAME
-pg_restore -p ${SRC_DB_PORT} -d ${SRC_DB_NAME} dumpfile 
+pg_restore -p ${SRC_DB_PORT} -d ${SRC_DB_NAME} megdb.dump 
 psql -p ${SRC_DB_PORT} -f after_pg_restore_test_data.sql $SRC_DB_NAME
