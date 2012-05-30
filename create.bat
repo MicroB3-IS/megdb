0ac83ce441fb205091a0622a1d618bc8b18d7e43
renzo <renzok@users.noreply.github.com>
2012-05-30 00:22:30 +0000

    windows re-creation of megdb nearly finished (only one error left, which can be ignored)

--- create.bat
+++ create.bat
@@ -3,23 +3,31 @@
 :: 2. postgres -D [myclusterdir]
 :: 3. psql -p [port] -c "create database [DATABASE];"
 :: 4. create.sh
-:: psql has to be in PATH
-
+:: the postgresql bin has to be in PATH
+@ECHO off
 ECHO PATH=%PATH%
 
 SET RELEASE=9
 SET VERSIONING=/opt/versioning
-SET POSTGIS=/opt/postgres/9.1.1/pgsql/share/contrib/postgis-1.5
+SET POSTGIS=E:\dev\pg-91\share\contrib\postgis-1.5
 SET PORT=5432
 SET DATABASE=megdb-%RELEASE%
+SET PSQL_ARGS=%PSQL_ARGS% -v ON_ERROR_STOP=1 -U postgres 
 
-psql -p %PORT% -f %VERSIONING%/install.versioning.sql %DATABASE%
-psql -p %PORT% -f %POSTGIS%/postgis.sql %DATABASE%
-psql -p %PORT% -f patches+/hstore+.sql %DATABASE%
-psql -p %PORT% -f patches+/roles+.sql %DATABASE%
-psql -p %PORT% -f patches+/baseline+.sql %DATABASE%
-psql -p %PORT% -f patches+/1-partitioning+.sql %DATABASE%
-psql -p %PORT% -f patches+/5-view-fix+.sql %DATABASE%
-psql -p %PORT% -f patches+/6-drop-sequence-storage-legacy+.sql %DATABASE%
+
+createdb -E UTF8 %DATABASE%
+
+PAUSE
+
+psql %PSQL_ARGS% -f patches/add-versioning.sql %DATABASE%
+psql %PSQL_ARGS% -f %POSTGIS%/postgis.sql %DATABASE%
+psql %PSQL_ARGS% -f %POSTGIS%/spatial_ref_sys.sql %DATABASE%
+psql %PSQL_ARGS% -f %POSTGIS%/postgis_comments.sql %DATABASE%
+psql %PSQL_ARGS% -f patches/hstore.sql %DATABASE%
+psql %PSQL_ARGS% -f patches/roles.sql %DATABASE%
+psql %PSQL_ARGS% -f patches/baseline.sql %DATABASE%
+psql %PSQL_ARGS% -f patches/1-partitioning.sql %DATABASE%
+psql %PSQL_ARGS% -f patches/5-view-fix.sql %DATABASE%
+psql %PSQL_ARGS% -f patches/6-drop-sequence-storage-legacy.sql %DATABASE%
 
 PAUSE
--- create.sh
+++ create.sh
@@ -9,11 +9,11 @@ POSTGIS=/opt/postgres/9.1.1/pgsql/share/contrib/postgis-1.5
 PORT=5432
 DATABASE=megdb
 
-psql -p ${PORT} -f ${VERSIONING}/install.versioning.sql ${DATABASE}
+psql -p ${PORT} -f patches/add-versioning.sql ${DATABASE}
 psql -p ${PORT} -f ${POSTGIS}/postgis.sql ${DATABASE}
-psql -p ${PORT} -f patches+/hstore+.sql ${DATABASE}
-psql -p ${PORT} -f patches+/roles+.sql ${DATABASE}
-psql -p ${PORT} -f patches+/baseline+.sql ${DATABASE}
-psql -p ${PORT} -f patches+/1-partitioning+.sql ${DATABASE}
-psql -p ${PORT} -f patches+/5-view-fix+.sql ${DATABASE}
-psql -p ${PORT} -f patches+/6-drop-sequence-storage-legacy+.sql ${DATABASE}
+psql -p ${PORT} -f patches/hstore.sql ${DATABASE}
+psql -p ${PORT} -f patches/roles.sql ${DATABASE}
+psql -p ${PORT} -f patches/baseline.sql ${DATABASE}
+psql -p ${PORT} -f patches/1-partitioning.sql ${DATABASE}
+psql -p ${PORT} -f patches/5-view-fix.sql ${DATABASE}
+psql -p ${PORT} -f patches/6-drop-sequence-storage-legacy.sql ${DATABASE}
