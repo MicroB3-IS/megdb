70d48b7c22929b1f4b39cb77c01937061620e23b
renzo <renzok@users.noreply.github.com>
2012-06-17 12:28:58 +0000

    better articles and samples tables

---
+++ patches/10-improve-samples-studies.sql
@@ -0,0 +1,23 @@
+﻿
+BEGIN;
+
+SELECT _v.register_patch( '10-improve-samples-studies', ARRAY['9-improve-article-table'], NULL );
+
+set client_encoding = 'UTF8';
+
+INSERT INTO core.studies(
+            label, full_name)
+    VALUES ('unknown', 'nothing is known about the study at the time the entry was created'),
+            ('metagenome', 'an anonymous metagenome study')
+    ;
+
+
+ALTER TABLE core.samples
+   ALTER COLUMN study SET DEFAULT 'unknown'::text;
+COMMENT ON COLUMN core.samples.study IS 'the study this sample primarily belongs to';
+
+ALTER TABLE core.samples DROP CONSTRAINT samples_own_fkey;
+ALTER TABLE core.samples DROP CONSTRAINT samples_project_fkey;
+
+
+commit;
