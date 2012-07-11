814498be4e84779b6d6936ead69bd87e4cdbf603
pjonoski <pjonoski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-07-11 10:15:35 +0000

    Initial protected resources.

---
+++ patches/10-authdb-web_resource_roles.sql
@@ -0,0 +1,25 @@
+begin;
+
+SELECT _v.register_patch( '10-authdb-web_resource_roles', ARRAY['9-improve-article-table'], NULL );
+
+
+update auth.users
+	set pass=md5('megx')
+		where logname='megx';
+		
+delete from auth.web_resource_permissions;
+
+insert into auth.web_resource_permissions(
+            url_path, http_method, role)
+    values 
+    	('/apps*', 'all', 'user'),
+    	('/apps*', 'all', 'admin'), -- the apps manager
+    	('/admin/*', 'all', 'admin'), -- the admin part
+    	('/ws/apps*', 'all', 'user'),
+    	('/ws/apps*', 'all', 'admin'), -- the apps REST services 
+		('/oauth/authorize*','all','admin'),
+		('/oauth/authorize*','all','user'), -- Authorization endpoint must be available only for logged in users
+		('/security/*','all','admin'), -- Must be admin to access security management interface
+		('/ws/filter/*','all', 'admin'); -- must be admin to use the Users/Roles/WebResources admin REST services
+
+commit;
