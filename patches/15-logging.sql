dc880b6fd386ea17926cc53f41dda67ddcc23f42
pjonoski <pjonoski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-12-18 17:28:48 +0000

    Logging database schema and model added

---
+++ patches/15-logging.sql
@@ -0,0 +1,27 @@
+begin;
+
+drop schema if exists logging cascade;
+create schema logging;
+
+alter schema logging OWNER to megdb_admin;
+set search_path = logging, public, pg_catalog;
+set default_tablespace = '';
+set default_with_oids = false;
+
+drop  table if exists errors;
+create table errors(
+	id text not null primary key, -- nonce
+	http_code integer not null default 500,
+	"time" timestamp with time zone,
+	message text,
+	request_uri text,
+	stack_trace text,
+	java_type text,
+	"user" text, -- user id
+	remote_ip text,
+	feedback text
+);
+
+
+
+commit;
