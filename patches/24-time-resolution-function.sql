ad3f7004b4c28b4b9b4fc19dd5b3ddc8f36d8f47
mschneid <mschneid@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-04-10 20:55:05 +0000

    -resolve ticket MB3_IS-214

---
+++ patches/24-time-resolution-function.sql
@@ -0,0 +1,31 @@
+begin;
+
+SELECT _v.register_patch( '24-time-resolution-function', ARRAY[''], NULL );
+
+CREATE FUNCTION core.get_time_resolution(input TIMESTAMP) RETURNS TEXT AS $$
+DECLARE
+  result TEXT;
+  hour INT;
+  minute INT;
+  second FLOAT;
+BEGIN
+  hour := EXTRACT(hour from input)::INT;
+  minute := EXTRACT(minute from input)::INT;
+  second := EXTRACT(second from input)::INT;
+  CASE
+    WHEN input = 'infinity' THEN
+      result := 'infinity';
+    WHEN hour = 0 AND minute = 0 AND second = 0 THEN
+      result := 'day';
+    WHEN minute = 0 AND second = 0 THEN
+      result := 'hour';
+    WHEN second = 0 THEN
+      result := 'minute';
+    ELSE
+      result := 'second';
+  END CASE;
+  RETURN RESULT;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+commit;
