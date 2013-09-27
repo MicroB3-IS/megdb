14fbe7e1c5b7387752f34e47fae6f7486603a88d
renzo <renzok@users.noreply.github.com>
2013-09-27 12:14:48 +0000

    firstdraft of new service table for mg traits

---
+++ patches/31-mg-traits.sql
@@ -0,0 +1,42 @@
+
+BEGIN;
+
+CREATE schema mg_traits;
+
+SET search_path to mg_traits,core;
+
+CREATE TABLE mg_traits_jobs (
+  customer text REFERENCES auth.users(logname),
+  mg_url text UNIQUE,
+  sample_label text UNIQUE,
+  sample_environment text,
+  time_submitted timestamp with time zone NOT NULL DEFAULT now(), -- date and time of job submission
+  time_finished timestamp with time zone  NOT NULL DEFAULT 'infinity'::timestamp, -- date and time of job finished
+  PRIMARY KEY (mg_url, sample_label)
+);
+
+
+select pgq.create_queue('clusterjobq');
+
+CREATE TRIGGER mg_traits_jobs_i_to_queue
+  AFTER INSERT
+  ON mg_traits.mg_traits_jobs
+  FOR EACH ROW
+  EXECUTE PROCEDURE pgq.logutriga('clusterjobq');
+
+
+CREATE TABLE mg_traits_results (
+   sample_label text REFERENCES mg_traits_jobs(sample_label),
+   gc_content numeric NOT NULL DEFAULT 'NaN',
+   gc_variance numeric NOT NULL DEFAULT 'NaN',
+   PRIMARY KEY(sample_label)
+);
+
+
+-- testing
+
+INSERT INTO  mg_traits_jobs VALUES ('anonymous', 'http://www.megx.net','test-sample', 'marine');
+
+ROLLBACK;
+
+
