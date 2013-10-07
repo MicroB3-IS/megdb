9d437d59581297f72d2c76c927a0127491c6f6ae
mschneid <mschneid@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-10-07 10:12:16 +0000

    pgq updated

--- pgq/antares/install.sh
+++ pgq/antares/install.sh
@@ -2,3 +2,4 @@
 
 cp pgq /lib/svc/method/
 cp pgq.xml /var/svc/manifest/application/database
+svccfg import /var/svc/manifest/application/database/pgq.xml
--- pgq/qsub-consumer.py
+++ pgq/qsub-consumer.py
@@ -10,17 +10,29 @@ class QsubConsumer(pgq.Consumer):
 		#	ev_data: column values urlencoded, to decode
 		#		d = skytools.db_urldecode(ev.ev_data)
 		#	ev_extra1: table name
+
+		# extract connection parameters from src_db, src_db is configured in the consumer .conf file
+        	connection_parameters = dict(token.split('=') for token in shlex.split(src_db.dsn))
+
+		# pass connection parameters to submitted script
 		p = "qsub -v"
-		p += " db_host=%s" % (src_db.host)
-		p += ",db_port=%s" % (src_db.port)
-		p += ",db_name=%s" % (src_db.dbname)
+		p += " db_host=%s" % (connection_parameters['host'])
+		p += ",db_port=%s" % (connection_parameters['port'])
+		p += ",db_name=%s" % (connection_parameters['dbname'])
+
+		# the table that triggered the event decides which script to submit
+		# all SGE params are in the script
 		if ( ev.ev_extra1 == 'blast_run'):
-			p = "qsub ~/pgq/blastc.sh '%s'" % (ev.ev_data) # all SGE params are in the script
+			self.log.info ("Consuming event triggered by INSERT on 'blast_run' table")
+			p = "qsub ~/pgq/blastc.sh '%s'" % (ev.ev_data) 
 		elif (ev.ev_extra1 == 'mg_traits_jobs'):
+			self.log.info ("Consuming event triggered by INSERT on 'mg_traits_jobs' table")
 			p = "qsub ~/pgq/traits_calc.sh '%s'" % (ev.ev_data) # all SGE params are in the script
 		else:
-			self.log.error ("table '%s' is not supported by qsub consumer\n" % (ev.ev_extra1))
+			self.log.info ("table '%s' is not supported by qsub consumer. Event will be ignored.\n" % (ev.ev_extra1))
 			p = "echo"
+
+		# submit script
 		output = subprocess.Popen(p, shell=True, executable="/bin/bash", stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 		self.log.info ("Executing...\n%s\nSTDOUT: %s\nSTDERR: %s\n" % (p,output[0],output[1]))
 		ev.tag_done()
