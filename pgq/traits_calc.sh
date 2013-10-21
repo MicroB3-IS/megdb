1388c6381c1d2caf57b989e770941029877773d8
mschneid <mschneid@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-10-21 14:37:47 +0000

    -new version of traits_calc -parameters will be passed

--- pgq/traits_calc.sh
+++ pgq/traits_calc.sh
@@ -16,10 +16,29 @@
 #example from blast consumer;
 #echo $(psql -U sge -h $db_host -p $db_port -d $db_name -c "COPY(select seq from core.blast_run where sid='$sid' AND jid='$jid'::numeric) TO STDOUT") | eval blastall -p $prog -d /vol/biodb/megx/$db $evalue -m 7 > $outfile
 
+string=$(echo $1 | sed -e 's/&/|/g' -e 's/%2d/-/g' -e 's/%2e/\./g' -e 's/%5f/_/g' -e 's/%3a/:/g' -e 's/\+/ /g')
+
+IFS="|"
+
+for pair in $string; do
+key=${pair%=*}
+value=${pair#*=}
+
+echo $key -- $value;
+
+if [ "$key" = "sample_label" ]; then
+	SAMPLE=$value;
+fi
+
+if [ "$key" = "mg_url" ]; then
+	FILE=$value;
+fi
+
+done
 
 # READ PROPERTIES FILE
-FILE=$1
-
+#FILE=$1
+#SAMPLE=$2
 #source "./"$PROPS
 #if [ "$?" -ne "0" ]; then
 #  echo "Validation failed."
@@ -123,11 +142,19 @@ printf "Number of bases: %d\nGC content: %f\nGC variance: %f\n" $NUMBASES $GC $V
 
 
 # Get ORFS with getorf
-printf "Getting ORFs..."
-ORFAA=$PREFIX"_orf_aa.fasta"
-ORFNT=$PREFIX"_orf_nt.fasta"
-getorf -minsize 180 $FILE -auto -find 0 -outseq $ORFAA
-getorf -minsize 180 $FILE -auto -find 2 -outseq $ORFNT
+GENEAA=$PREFIX"_gene_aa.fasta"
+GENENT=$PREFIX"_gene_nt.fasta"
+NSEQ=$[($NUMREADS/8)+1]
+#Split original in as many files as cores
+printf "Splitting file ($NSEQ seqs file)..."
+awk -vO=$NSEQ 'BEGIN {n_seq=0;} /^>/ {if(n_seq%O==0){file=sprintf("myseq%d.fa",n_seq);} print >> file; n_seq++; next;} { print >> file; }' < $FILE
+printf " done\n"
+
+printf "Gene calling..."
+~/opt/parallel/bin/parallel -j 8 'FGS/run_FragGeneScan.pl -genome={} -out={.}.genes10 -complete=0 -train=sanger_10' ::: *.fa
+
+cat myseq*.faa > $GENEAA
+cat myseq*.ffn > $GENENT
 
 if [ "$?" -ne "0" ]; then
   echo "Something went wrong."
@@ -135,14 +162,14 @@ if [ "$?" -ne "0" ]; then
 fi
 printf " done\n"
 
-NUMORFS=$(grep -c '>' $ORFAA)
+NUMGENES=$(grep -c '>' $GENENT)
 
-printf "Number of ORFs: %d\n" $NUMORFS
+printf "Number of geness: %d\n" $NUMGENES
 
 
 # Functional annotation
 printf "Getting functional profile..."
-sh ~/upro/beta/upro.sh < $FILE | awk '{FS=",";printf("PF%05d\n", $4)}' > pfam.txt
+sh ~/upro/beta/upro.sh < $GENENT | awk '{FS=",";printf("PF%05d\n", $4)}' > pfam.txt
 
 if [ "$?" -ne "0" ]; then
   echo "Something went wrong."
@@ -155,8 +182,12 @@ t.t<-as.data.frame(table(t))
 colnames(p)<-'t'
 t.m<-merge(t.t, p, all = T, by= "t")
 t.m[is.na(t.m)]<-0
-write.table(t.m, file = 'functional_table.txt', sep = "\t", row.names = F, quote = F, header = F)
+write.table(t.m, file = 'functional_table.txt', sep = "\t", row.names = F, quote = F, col.names = F)
 RSCRIPT
+awk -v f=$SAMPLE 'BEGIN{ORS="";print "f\t{"}{ if (NR == 1) {print "{"$1","$2"}"}else{print ",{"$1","$2"}"}}END{print "}"}' functional_table.txt > pfam_db.txt
+
+cusp --auto -stdout $GENENT |awk '{if ($0 !~ "*" && $0 !~ /[:alphanum:]/ && $0 !~ /^$/){ print $1,$2,$5}}' > codon.cusp
+
 
 if [ "$?" -ne "0" ]; then
   echo "Something went wrong."
@@ -164,7 +195,7 @@ if [ "$?" -ne "0" ]; then
 fi
 printf " done\n"
 R --vanilla <<'RSCRIPT' > /dev/null
-codon<-read.table(file = "GS002.cusp", header = F, stringsAsFactors = F, sep = ' ')
+codon<-read.table(file = "codon.cusp", header = F, stringsAsFactors = F, sep = ' ')
 codon<-cbind(codon, codon$V3/sum(codon$V3))
 colnames(codon)<-c("codon", "aa", "raw", "prop")
 aa<-aggregate(raw ~ aa, data = codon, sum)
@@ -173,8 +204,48 @@ colnames(aa)<-c("aa", "raw", "prop")
 aa2<-as.data.frame(t(aa$prop))
 colnames(aa2)<-aa$aa
 ab<-(aa2$D + aa2$E)/(aa2$H + aa2$R + aa2$K)
+write.table(aa2, file = 'aa_table.txt', sep = "\t", row.names = F, quote = F, col.names  = T)
+write(ab, file = 'ab_ratio.txt')
+RSCRIPT
+ABRATIO=$(cat ab_ratio.txt)
 
-write.table(t.m, file = 'functional_table.txt', sep = "\t", row.names = F, quote = F, header = T)
-write.t
-able(t.m, file = 'functional_table.txt', sep = "\t", row.names = F, quote = F, header = F)
+compseq --auto -stdout -word 1 GS002.fna |awk '{if (NF == 5 && $0 ~ /^A|T|C|G/ && $0 !~ /[:alphanum:]/ ){print $1,$2,$3}}' > nuc_freqs.txt
+
+compseq --auto -stdout -word 2 GS002.fna |awk '{if (NF == 5 && $0 ~ /^A|T|C|G/ && $0 !~ /[:alphanum:]/ ){print $1,$2,$3}}' > dinuc_freqs.txt
+
+R --vanilla <<'RSCRIPT' > /dev/null
+nuc<-read.table(file = "nuc_freqs.txt", header = F, stringsAsFactors = F, sep = ' ')
+rownames(nuc)<-nuc$V1
+nuc$V1<-NULL
+nuc<-as.data.frame(t(nuc))
+dinuc<-read.table(file = "dinuc_freqs.txt", header = F, stringsAsFactors = F, sep = ' ')
+rownames(dinuc)<-dinuc$V1
+dinuc$V1<-NULL
+dinuc<-as.data.frame(t(dinuc))
+#Forward strand f(X) when X={A,T,C,G} in S
+fa<-nuc$A[[2]]
+ft<-nuc$T[[2]]
+fc<-nuc$C[[2]]
+fg<-nuc$G[[2]]
+#Frequencies when S + SI = S*; f*(X) when X= {A,T,C,G}
+faR<-(fa+ft)/2
+fcR<-(fc+fg)/2
+fAA <- (dinuc$AA[[2]] + dinuc$TT[[2]])/2
+fAC <- (dinuc$AC[[2]] + dinuc$GT[[2]])/2
+fCC <- (dinuc$CC[[2]] + dinuc$GG[[2]])/2
+fCA <- (dinuc$CA[[2]] + dinuc$TG[[2]])/2
+fGA <- (dinuc$GA[[2]] + dinuc$TC[[2]])/2
+fAG <- (dinuc$AG[[2]] + dinuc$CT[[2]])/2
+pAA <- fAA/(faR * faR)
+pAC <- fAC/(faR * fcR)
+pCC <- fCC/(fcR * fcR)
+pCA <- fCA/(faR * fcR)
+pGA <- fGA/(faR * fcR)
+pAG <- fAG/(faR * fcR)
+pAT <- dinuc$AT[[2]]/(faR * faR)
+pCG <- dinuc$CG[[2]]/(fcR * fcR)
+pGC <- dinuc$GC[[2]]/(fcR * fcR)
+pTA <- dinuc$TA[[2]]/(faR * faR)
+odds<-cbind(pAA, pAC, pCC, pCA, pGA, pAG, pAT, pCG, pGC, pTA)
+colnames(odds)<-c("pAA/pTT", "pAC/pGT", "pCC/pGG", "pCA/pTG", "pGA/pTC", "pAG/pCT", "pAT", "pCG", "pGC", "pTA")
 RSCRIPT
