b25c6d6e8b364001fb133f0ab38cc7251d3fd50d
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-12-10 18:18:36 +0000

    Changed osd registry root url from osd to osd-registry as per discussion with Renzo

---
+++ patches/41-osd-registry-url-change.sql
@@ -0,0 +1,9 @@
+begin;
+
+SELECT _v.register_patch( '41-osd-registry-url-change.sql', ARRAY['8-authdb'], NULL );
+
+UPDATE auth.web_resource_permissions
+SET url_path='/osd-registry/add*'
+WHERE url_path='/osd/add*'
+
+commit;
