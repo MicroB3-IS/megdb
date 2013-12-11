84c59e35cb5e349c18464aea585811bf62495dc2
renzo <renzok@users.noreply.github.com>
2013-12-11 12:35:16 +0000

    corrected wrongly committed script

--- patches/40-remove-esa-projects.sql
+++ patches/40-remove-esa-projects.sql
@@ -3,6 +3,6 @@ begin;
 SELECT _v.register_patch( '40-remove-esa-projects.sql', ARRAY['14-esa-demo'], NULL );
 
 DELETE FROM esa.gen_config
-WHERE category='projects' AND name='biovel'
+WHERE category='projects' AND name='biovel';
 
 commit;
