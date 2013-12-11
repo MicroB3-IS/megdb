61474879bb34d1706057282510b19af243956333
renzo <renzok@users.noreply.github.com>
2013-12-11 12:37:36 +0000

    corrected wrongly committed script

--- patches/41-osd-registry-url-change.sql
+++ patches/41-osd-registry-url-change.sql
@@ -4,6 +4,6 @@ SELECT _v.register_patch( '41-osd-registry-url-change.sql', ARRAY['8-authdb'], N
 
 UPDATE auth.web_resource_permissions
 SET url_path='/osd-registry/add*'
-WHERE url_path='/osd/add*'
+WHERE url_path='/osd/add*';
 
 commit;
