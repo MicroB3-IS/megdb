4730fa7e057d8b4c0aa987ab2e322c86bfd19fb5
renzo <renzok@users.noreply.github.com>
2014-02-05 03:21:12 +0000

    first examples for mg-traits-module

---
+++ examples/mg-traits-exmaple-queries.sql
@@ -0,0 +1,33 @@
+BEGIN;
+
+set search_path to mg_traits;
+
+
+-- post on /jobs
+-- example started(ing) job
+-- customer as by authorization or anonymous
+INSERT INTO mg_traits_jobs (
+    customer, mg_url, sample_label, sample_environment
+  ) 
+  VALUES (
+    'mg-traits-tester', 
+    'https://dev-dav.megx.net/test-data/mg-traits-test2.fasta',
+    'running job sample', 'marine'
+) RETURNING id;
+
+
+-- // get on /mg{id}:{sample_name}/simple-traits
+select * from mg_traits_results where id = ?;
+
+-- // get on /mg{id}:{sample_name}/function-table
+select * from mg_traits_pfam where id = ?;
+
+-- // get on /mg{id}:{sample_name}/amino-acid-content
+select * from mg_traits.aa where id = ?;
+
+-- // get on /mg{id}:{sample_name}/di-nucleotide-odds-ratio
+select * from mg_traits.dinuc where id = ?;
+
+
+
+rollback;
