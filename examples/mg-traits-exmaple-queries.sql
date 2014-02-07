a0ed5e13ff48def78b272601631f85a8fd44e9b2
renzo <renzok@users.noreply.github.com>
2014-02-07 01:01:44 +0000

    clarified more on spec. especially job related resources

--- examples/mg-traits-exmaple-queries.sql
+++ examples/mg-traits-exmaple-queries.sql
@@ -15,6 +15,8 @@ INSERT INTO mg_traits_jobs (
     'running job sample', 'marine'
 ) RETURNING id;
 
+-- get job details on /jobs/mg{id}:{sample_name}
+select id, sample_label, time_submitted, time_finished, return_code, error_message from mg_traits.mg_traits_jobs;
 
 -- // get on /mg{id}:{sample_name}/simple-traits
 select * from mg_traits_results where id = ?;
