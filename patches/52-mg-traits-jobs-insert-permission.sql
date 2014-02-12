f481781b0af8e54235aa4e322d2876475a4ae446
renzo <renzok@users.noreply.github.com>
2014-02-12 12:43:46 +0000

    added permissions for sge

--- patches/52-mg-traits-jobs-insert-permission.sql
+++ patches/52-mg-traits-jobs-insert-permission.sql
@@ -6,6 +6,8 @@ SELECT _v.register_patch('52-mg-traits-jobs-insert-permission',
 set search_path to mg_traits;	 
 
 GRANT insert ON mg_traits_jobs TO megxuser;
-GRANT usage, select on mg_traits_jobs_id_seq to megxuser;
+GRANT usage, select ON mg_traits_jobs_id_seq TO megxuser;
+GRANT select,update ON mg_traits_jobs TO sge;
+GRANT usage, select ON mg_traits_jobs_id_seq TO sge;
 
 commit;
