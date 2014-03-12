10307e509770454ab9995a892a5a0b85b258faf1
renzo <renzok@users.noreply.github.com>
2014-03-12 19:13:27 +0000

    applied patch 61-core-biodb-table.sql:

--- patches/61-core-biodb-table.sql
+++ patches/61-core-biodb-table.sql
@@ -1,6 +1,7 @@
 Begin;
+
 SELECT _v.register_patch('61-core-biodb-table',
-                          array['60-mg-traits-pk-pca-table'] );
+                          array['60-mg-traits-pk-pca-table.sql'] );
 
 --table for biodb
 CREATE TABLE core.biodb (
@@ -18,7 +19,17 @@ GRANT SELECT ON TABLE core.biodb TO selectors;
 GRANT SELECT ON TABLE core.biodb TO megxuser;
 GRANT SELECT, INSERT ON TABLE core.biodb TO sge;
 COMMENT ON TABLE core.biodb IS 'List of name databases as distributed in the cluster.';
-INSERT INTO core.biodb (label) VALUES ('pfam'), ('unknowns'), ('genomes'), ('gos'), ('marine_phages'), ('silva_lsu'), ('silva_ssu'); 
+INSERT INTO core.biodb (label,descr) VALUES 
+('pfam','database of protein families');
+
+INSERT INTO core.biodb (label,cat,descr) 
+    VALUES 
+('unknowns', 'blast', 'Based on unknown networks by Fernandez-Guerra et al.'), 
+('prok_genomes', 'blast', 'Blast DB of archaeal and bacterial proteins'), 
+('gos', 'blast', 'Blast DB of all Global Ocean Survey (GOS) reads '), 
+('marine_phages', 'blast', 'Blast DB of marine phage proteins'), 
+('silva_lsu', 'blast', 'Blast DB of all SILVA Long Subunit (lsu) rDNAs'),
+('silva_ssu', 'blast', 'Blast DB of all SILVA Small Subunit (ssu) rDNAs'); 
 
 --table for biodb versions
 CREATE TABLE core.biodb_version (
@@ -36,7 +47,11 @@ GRANT SELECT ON TABLE core.biodb_version TO selectors;
 GRANT SELECT ON TABLE core.biodb_version TO megxuser;
 GRANT SELECT, INSERT ON TABLE core.biodb_version TO sge;
 COMMENT ON TABLE core.biodb_version IS 'List of versions for databases distributed in the cluster.';
-INSERT INTO core.biodb (label, ver) VALUES ('pfam', '26.0'), ('genomes','r8'), ('gos'), ('marine_phages','r8'), ('silva_lsu','r8'), ('silva_ssu'.'r8'); 
-INSERT INTO core.biodb (label, ver, descr)('unknowns', '24-02-2014_aa', 'UnknownDB built using PFAM 26.0 and HMMER3 against GOS.');
-rollback;
---commit;
+
+INSERT INTO core.biodb_version (label, ver) VALUES ('pfam', '26.0'), ('prok_genomes','r8'), ('gos', 'r8'), ('marine_phages','r8'), ('silva_lsu','r8'), ('silva_ssu','r8'); 
+INSERT INTO core.biodb_version (label, ver, descr) VALUES ('unknowns', '24-02-2014_aa', 'UnknownDB built using PFAM 26.0 and HMMER3 against GOS.');
+
+-- select * from core.biodb;
+-- select * from core.biodb_version;
+
+commit;
