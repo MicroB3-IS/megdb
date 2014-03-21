5cd73c482f26e1eddb88df318c02cf69710b168a
Antonio Fernandez-Guerra <1398625+genomewalker@users.noreply.github.com>
2014-03-21 14:57:26 +0000

    ¨Fixed column name.

--- patches/62-mg-traits-jobs-fix-updated-data-GS122a.sql
+++ patches/62-mg-traits-jobs-fix-updated-data-GS122a.sql
@@ -3,6 +3,6 @@ Begin;
 SELECT _v.register_patch('62-mg-traits-jobs-fix-updated-data-GS122a',
                           array['62-mg-traits-jobs-fix-updated-data'] );
 
-UPDATE mg_traits.mg_traits_jobs SET sample_description = 'GS122a - International waters between Madagascar and South Africa', sample_name = 'GS122a', sample_site_description = 'Open Ocean', sample_latitude = '-30.898333', sample_longitude = '40.420277', sample_label = 'JCVI_SMPL_GS122a' WHERE sample_label = 'JCVI_SMPL_GS122';
+UPDATE mg_traits.mg_traits_jobs SET sample_description = 'GS122a - International waters between Madagascar and South Africa', sample_name = 'GS122a', sample_environment = 'Open Ocean', sample_latitude = '-30.898333', sample_longitude = '40.420277', sample_label = 'JCVI_SMPL_GS122a' WHERE sample_label = 'JCVI_SMPL_GS122';
 
 commit;
