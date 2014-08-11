961b428d8f2129f33dbf5dde70e7d63da8f44ecc
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-08-11 13:03:19 +0000

    New contact table and new pubmap schema for bookmarklet.

---
+++ patches/83-new-pubmap-schema.sql
@@ -0,0 +1,32 @@
+BEGIN;
+
+SELECT _v.register_patch('83-new-pubmap-schema.sql','{}',NULL);
+
+CREATE SCHEMA pubmap AUTHORIZATION megdb_admin;
+
+GRANT ALL ON SCHEMA pubmap TO megdb_admin;
+GRANT ALL ON SCHEMA pubmap TO megxuser;
+GRANT ALL ON SCHEMA pubmap TO selectors;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA pubmap
+    GRANT SELECT ON TABLES
+    TO megxuser;
+    
+    
+CREATE TABLE pubmap.raw_pubmap
+(
+  pmid integer NOT NULL,
+  geom geometry,
+  article_xml xml DEFAULT '<e/>'::xml,
+  user_name text,
+  megxbar json,
+  created timestamp with time zone,
+  CONSTRAINT raw_pubmap_pkey PRIMARY KEY (pmid)
+);
+
+ALTER TABLE pubmap.raw_pubmap
+  OWNER TO postgres;
+GRANT ALL ON TABLE pubmap.raw_pubmap TO postgres;
+GRANT SELECT ON TABLE pubmap.raw_pubmap TO megxuser;  
+
+commit;
