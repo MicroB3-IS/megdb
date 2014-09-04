e0db3b051883eef4d72c97afe84366a2f2dd3f30
renzo <renzok@users.noreply.github.com>
2014-09-04 15:46:30 +0000

    more queries

--- examples/osd-registry-queries.sql
+++ examples/osd-registry-queries.sql
@@ -7,3 +7,12 @@ select * from osdregistry.osd_raw_samples order by submitted desc;
 
 -- give me institie and site id last submited first
 select submitted, raw_json #>> '{contact, institute}', raw_json #>> '{sampling_site, site_id}' from osdregistry.osd_raw_samples order by submitted desc;
+
+-- give me data, last submmited first
+select submitted, -- date submitted 
+       raw_json #>> '{contact, institute}', --from which institute 
+       raw_json #>> '{sampling_site, site_id}' as osd_id,
+       raw_json #>> '{sample, label}' as sample_label,
+       raw_json #>> '{sampling_site}',
+       raw_json #>> '{comment}' -- was there addtional comment
+  from osdregistry.osd_raw_samples order by submitted desc;
