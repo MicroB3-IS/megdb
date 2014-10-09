8bd0df14c0bba6df859a6282c4818c48e2dd4949
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-10-09 13:23:08 +0000

    MIBIG schema and submissions table patch edited

---
+++ patches/89-new-mibig-schema.sql
@@ -0,0 +1,30 @@
+﻿BEGIN;
+
+SELECT _v.register_patch('89-new-mibig-schema.sql','{}',NULL);
+
+CREATE SCHEMA mibig AUTHORIZATION megdb_admin;
+
+GRANT ALL ON SCHEMA mibig TO megdb_admin;
+GRANT ALL ON SCHEMA mibig TO megxuser;
+GRANT ALL ON SCHEMA mibig TO selectors;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA mibig
+    GRANT SELECT ON TABLES
+    TO megxuser;
+    
+    
+CREATE TABLE mibig.submissions
+(
+  id SERIAL PRIMARY KEY,
+  submitted timestamptz NOT NULL DEFAULT NOW(),
+  modified  timestamptz NOT NULL DEFAULT NOW(),
+  raw  json NOT NULL DEFAULT '{}',
+  v integer check (v > 0) 
+);
+
+ALTER TABLE mibig.submissions
+  OWNER TO postgres;
+GRANT ALL ON TABLE mibig.submissions TO postgres;
+GRANT SELECT ON TABLE mibig.submissions TO megxuser;  
+
+commit;
