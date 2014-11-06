a12b946daa43c60444ad520ad81ec442ee6137af
renzo <renzok@users.noreply.github.com>
2014-11-06 11:42:26 +0000

    osd patch preparation

--- test-osd-registry.sql
+++ test-osd-registry.sql
@@ -1,37 +1,282 @@
-
+﻿
 BEGIN;
 
-set search_path to osdregistry;
+set search_path to osdregistry,public;
+
+CREATE VIEW submission_overview AS
+   select
+        raw_json #>> '{sampling_site, site_id}' as osd_id,
+        raw_json #>> '{sampling_site, site_name}' as site_name,
+        version,
+        raw_json #>> '{sampling_site, marine_region}' as marine_region,
+        raw_json #>> '{sampling_site, start_coordinates,latitude}' as start_lat,
+        raw_json #>> '{sampling_site, start_coordinates,longitude}' as start_lon,
+        raw_json #>> '{sampling_site, stop_coordinates,latitude}' as stop_lat,
+        raw_json #>> '{sampling_site, stop_coordinates,longitude}' as stop_lon,
+        raw_json #>> '{sample, depth}' as sample_depth,
+        raw_json #>> '{sample, date}' as sample_date,
+        raw_json #>> '{contact, first_name}' as first_name,
+        raw_json #>> '{contact, last_name}' as last_name,
+        raw_json #>> '{contact, institute}' as institute,
+        raw_json #>> '{contact, email}' as email,
+        raw_json -> 'environment' ->> 'water_temperature' as water_temperature,
+        raw_json -> 'environment' ->> 'salinity' as salinity,
+        CASE WHEN 
+          raw_json #>> '{environment, ph, choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, ph, measurement, value}'
+        ELSE
+          raw_json #>> '{environment, ph, choice}'
+        END as ph,
+        CASE WHEN 
+          raw_json #>> '{environment, phosphate, choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, phosphate, measurement, value}'
+        ELSE
+          raw_json #>> '{environment, phosphate, choice}'
+        END as phospahte,
+       CASE WHEN 
+          raw_json #>> '{environment, nitrate, nitrate-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, nitrate, nitrate-measurement}'
+        ELSE
+          raw_json #>> '{environment,nitrate , nitrate-choice}'
+        END as nitrate,
+        CASE WHEN 
+          raw_json #>> '{environment, carbon_organic_particulate_poc, carbon_organic_particulate_poc-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, carbon_organic_particulate_poc, carbon_organic_particulate_poc-measurement}'
+        ELSE
+          raw_json #>> '{environment, carbon_organic_particulate_poc, carbon_organic_particulate_poc-choice}'
+        END as carbon_organic_particulate,
+        CASE WHEN 
+          raw_json #>> '{environment, nitrite, nitrite-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, nitrite, nitrite-measurement}'
+        ELSE
+          raw_json #>> '{environment, nitrite, nitrite-choice}'
+        END as nitrite,
+        CASE WHEN 
+          raw_json #>> '{environment, carbon_organic_dissolved_doc, carbon_organic_dissolved_doc-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, carbon_organic_dissolved_doc, carbon_organic_dissolved_doc-measurement}'
+        ELSE
+          raw_json #>> '{environment, carbon_organic_dissolved_doc, carbon_organic_dissolved_doc-choice}'
+        END as carbon_organic_dissolved_doc,
+        CASE WHEN 
+          raw_json #>> '{environment, nano_microplankton, nano_microplankton-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, nano_microplankton, nano_microplankton-measurement}'
+        ELSE
+          raw_json #>> '{environment, nano_microplankton, nano_microplankton-choice}'
+        END as nano_microplankton,
+        CASE WHEN 
+          raw_json #>> '{environment, downward_par, downward_par-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, downward_par, downward_par-measurement}'
+        ELSE
+          raw_json #>> '{environment, downward_par, downward_par-choice}'
+        END as downward_par,
+        CASE WHEN 
+          raw_json #>> '{environment, conductivity, conductivity-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, conductivity, conductivity-measurement}'
+        ELSE
+          raw_json #>> '{environment, conductivity, conductivity-choice}'
+        END as conductivity,
+        CASE WHEN 
+          raw_json #>> '{environment, primary_production_isotope_uptake, primary_production_isotope_uptake-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, primary_production_isotope_uptake, primary_production_isotope_uptake-measurement}'
+        ELSE
+          raw_json #>> '{environment, primary_production_isotope_uptake, primary_production_isotope_uptake-choice}'
+        END as primary_production_isotope_uptake,
+        CASE WHEN 
+          raw_json #>> '{environment, primary_production_oxygen, primary_production_oxygen-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, primary_production_oxygen, primary_production_oxygen-measurement}'
+        ELSE
+          raw_json #>> '{environment, primary_production_oxygen, primary_production_oxygen-choice}'
+        END as primary_production_oxygen,
+        CASE WHEN 
+          raw_json #>> '{environment, dissolved_oxygen_concentration, dissolved_oxygen_concentration-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, dissolved_oxygen_concentration, dissolved_oxygen_concentration-measurement}'
+        ELSE
+          raw_json #>> '{environment, dissolved_oxygen_concentration, dissolved_oxygen_concentration-choice}'
+        END as dissolved_oxygen_concentration,
+        CASE WHEN 
+          raw_json #>> '{environment, nitrogen_organic_particulate_pon, nitrogen_organic_particulate_pon-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, nitrogen_organic_particulate_pon, nitrogen_organic_particulate_pon-measurement}'
+        ELSE
+          raw_json #>> '{environment, nitrogen_organic_particulate_pon, nitrogen_organic_particulate_pon-choice}'
+        END as nitrogen_organic_particulate_pon,
+        CASE WHEN 
+          raw_json #>> '{environment, meso_macroplankton, meso_macroplankton-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, meso_macroplankton, meso_macroplankton-measurement}'
+        ELSE
+          raw_json #>> '{environment, meso_macroplankton, meso_macroplankton-choice}'
+        END as meso_macroplankton,
+        CASE WHEN 
+          raw_json #>> '{environment, bacterial_production_isotope_uptake, bacterial_production_isotope_uptake-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, bacterial_production_isotope_uptake, bacterial_production_isotope_uptake-measurement}'
+        ELSE
+          raw_json #>> '{environment, bacterial_production_isotope_uptake, bacterial_production_isotope_uptake-choice}'
+        END as bacterial_production_isotope_uptake,
+        CASE WHEN 
+          raw_json #>> '{environment, nitrogen_organic_dissolved_don, nitrogen_organic_dissolved_don-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, nitrogen_organic_dissolved_don, nitrogen_organic_dissolved_don-measurement}'
+        ELSE
+          raw_json #>> '{environment, nitrogen_organic_dissolved_don, nitrogen_organic_dissolved_don-choice}'
+        END as nitrogen_organic_dissolved_don,
+        CASE WHEN 
+          raw_json #>> '{environment, ammonium, ammonium-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, ammonium, ammonium-measurement}'
+        ELSE
+          raw_json #>> '{environment, ammonium, ammonium-choice}'
+        END as ammonium,
+        CASE WHEN 
+          raw_json #>> '{environment, silicate, silicate-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, silicate, silicate-measurement}'
+        ELSE
+          raw_json #>> '{environment, silicate, silicate-choice}'
+        END as silicate,
+        CASE WHEN 
+          raw_json #>> '{environment, bacterial_production_respiration, bacterial_production_respiration-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, bacterial_production_respiration, bacterial_production_respiration-measurement}'
+        ELSE
+          raw_json #>> '{environment, bacterial_production_respiration, bacterial_production_respiration-choice}'
+        END as bacterial_production_respiration,
+        CASE WHEN 
+          raw_json #>> '{environment, turbidity, turbidity-choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, turbidity, turbidity-measurement}'
+        ELSE
+          raw_json #>> '{environment, turbidity, turbidity-choice}'
+        END as turbidity,
+        CASE WHEN 
+          raw_json #>> '{environment, fluorescence, choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, fluorescence, measurement, value}'
+        ELSE
+          raw_json #>> '{environment, fluorescence, choice}'
+        END as fluorescence,
+          CASE WHEN 
+          raw_json #>> '{environment, pigment_concentration, choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, pigment_concentration, measurement, value}'
+        ELSE
+          raw_json #>> '{environment, pigment_concentration, choice}'
+        END as pigment_concentration,
+          CASE WHEN 
+          raw_json #>> '{environment, picoplankton_flow_cytometry, choice}' = 'measured'
+        THEN
+          raw_json #>> '{environment, picoplankton_flow_cytometry, measurement, value}'
+        ELSE
+          raw_json #>> '{environment, picoplankton_flow_cytometry, choice}'
+        END as picoplankton_flow_cytometry,
+        
+        COALESCE ( (raw_json -> 'environment' ->> 'other_parameters'), 'not determinded') as other_params,
+
+        raw_json -> 'comment' as remarks 
+         
+   from osdregistry.osd_raw_samples 
+   WHERE (raw_json ->> 'version' = '6' OR raw_json ->> 'version' = '5') order by osd_id  desc
+;
+
+-- todo correct handling of version 5
+
+-- select start_lat, stop_lat, start_lon, stop_lon from submission_overview where version = 6;
+
 
 CREATE TABLE institutes (
   label text PRIMARY KEY,
-  country text,
-  homepage text
-  -- geom
-  -- accuracy
+  country_verb text NOT NULL DEFAULT '',
+  country text REFERENCES elayers.boundaries (terr_name),
+  homepage text check (homepage like 'http://%'),
+  max_uncertain numeric NOT NULL DEFAULT 'nan'::numeric
 );
 
+SELECT AddGeometryColumn(
+  'institutes',
+  'geom',
+   4326,
+  'POINT',
+  2
+);
 
-CREATE TABLE partners (
+-- todo add institute geo-refrence table
+
+
+INSERT 
+  INTO institutes (label, geom,country_verb) 
+SELECT DISTINCT ON (institution)
+       institution,  
+       institution_geom,
+       country  
+  FROM osd_participants;
+
+
+CREATE TABLE participants (
   email text PRIMARY KEY,
-  first_name text,
-  last_name text
+  first_name text NOT NULL,
+  last_name text NOT NULL
 );
+
+INSERT 
+  INTO participants (email, first_name, last_name) 
+SELECT DISTINCT ON (email)
+       email, first_name, last_name
+  FROM submission_overview;
+
+
+
 
 CREATE TABLE works_for (
-  email text REFERENCES partners(email),
-  institute text REFERENCES institutes(label)
+  email text REFERENCES participants(email),
+  institute text REFERENCES institutes(label),
+  PRIMARY KEY (email,institute)
 );
+
+/*
+SELECT osd_id, institute
+  FROM submission_overview;
+--*/
+
+INSERT 
+  INTO works_for (email, institute) 
+SELECT DISTINCT ON (email)
+       email, institute
+  FROM submission_overview;
+
 
 CREATE TABLE sites (
   id integer , 
-  label text
+  label text NOT NULL DEFAULT '',
+  label_verb text NOT NULL
   -- geom
   -- accuracy
 );
 
 -- not sure about this one, basically about wether OSD1014 or other...
-CREATE TABLE campaign ();
+CREATE TABLE campaigns (
+  label text PRIMARY KEY 
+);
+
+INSERT INTO campaigns 
+     VALUES ('OSD-Jun-2012'),('OSD-Dec-2012'),
+            ('OSD-Jun-2013'),('OSD-Dec-2013'),
+            ('OSD-Jun-2014'),('OSD-Dec-2014'),
+            ('OSD-Jun-2015'), ('OSD-Dec-2015'), 
+            ('OSD-Jun-2016'), ('OSD-Dec-2017');
+
 
 CREATE TABLE samples (
   id SERIAL UNIQUE,
@@ -46,28 +291,16 @@ CREATE TABLE samples (
 
 CREATE TABLE owned_by (
   label text REFERENCES samples(label),
-  email text REFERENCES partners(email),
+  email text REFERENCES participants(email),
   seq_author_order integer check(seq_author_order > 0)
 );
 
 
-CREATE OR REPLACE function integrate_sample_submission(sub json) RETURNS void AS $$
-  DECLARE   
-    test text;
-
-  BEGIN
-   test := sub #>>  '{environment,ph}';
-   RAISE NOTICE 'test=%', test;
-
-  END;
-$$ LANGUAGE plpgsql;
+CREATE TABLE filters (
+  label text
+);
 
 
-
-
-
-
-select integrate_sample_submission ( '{"contact":{},"sampling_site":{},"sample":{"filtration_time":-1},"environment":{"temperature":-1,"salinity":-1,"phosphate":{"phosphate-choice":"not determined"},"ph":{"ph-choice":"not determined"},"nitrate":{"nitrate-choice":"not determined"},"carbon_organic_particulate_poc":{"carbon_organic_particulate_poc-choice":"not determined"},"nitrite":{"nitrite-choice":"not determined"},"carbon_organic_dissolved_doc":{"carbon_organic_dissolved_doc-choice":"not determined"},"nano_microplankton":{"nano_microplankton-choice":"not determined"},"downward_par":{"downward_par-choice":"not determined"},"conductivity":{"conductivity-choice":"not determined"},"primary_production_isotope_uptake":{"primary_production_isotope_uptake-choice":"not determined"},"primary_production_oxygen":{"primary_production_oxygen-choice":"not determined"},"dissolved_oxygen_concentration":{"dissolved_oxygen_concentration-choice":"not determined"},"nitrogen_organic_particulate_pon":{"nitrogen_organic_particulate_pon-choice":"not determined"},"meso_macroplankton":{"meso_macroplankton-choice":"not determined"},"bacterial_production_isotope_uptake":{"bacterial_production_isotope_uptake-choice":"not determined"},"nitrogen_organic_dissolved_don":{"nitrogen_organic_dissolved_don-choice":"not determined"},"ammonium":{"ammonium-choice":"not determined"},"silicate":{"silicate-choice":"not determined"},"bacterial_production_respiration":{"bacterial_production_respiration-choice":"not determined"},"turbidity":{"turbidity-choice":"not determined"},"fluorescence":{"fluorescence-choice":"not determined"},"pigment_concentrations":{"pigment_concentrations-choice":"not determined"},"picoplankton_flow_cytometry":{"picoplankton_flow_cytometry-choice":"not determined"}}}'::json);
 
 
 ROLLBACK;
