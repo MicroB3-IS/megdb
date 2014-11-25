5d38860247bc1ff589cba9b069cd1697c98f9851
renzo <renzok@users.noreply.github.com>
2014-11-25 10:22:15 +0000

    fixed missing schema qualification

--- patches/00098-mibig-gene-npkrs-tables-patch.sql
+++ patches/00098-mibig-gene-npkrs-tables-patch.sql
@@ -1,13 +1,14 @@
 
 BEGIN;
 SELECT _v.register_patch('00098-mibig-gene-npkrs-tables',
-                          array['00097-mg-traits-test-friendly-cnstrnt-patch'] );
+                          array['00097-mg-traits-test-friendly-cnstrnt'] );
 
 -- section of creation best as user role megdb_admin
 SET ROLE megdb_admin;
 
 
-CREATE TABLE gene_submissions (
+
+CREATE TABLE mibig.gene_submissions (
   id SERIAL PRIMARY KEY,
   bgc_id text NOT NULL default 'BGC00000',
   submitted timestamp with time zone NOT NULL default now(),
@@ -17,7 +18,7 @@ CREATE TABLE gene_submissions (
 );
 
 
-CREATE TABLE nrps_submissions (
+CREATE TABLE mibig.nrps_submissions (
   id SERIAL PRIMARY KEY,
   bgc_id text NOT NULL default 'BGC00000',
   submitted timestamp with time zone NOT NULL default now(),
@@ -25,15 +26,10 @@ CREATE TABLE nrps_submissions (
   raw json NOT NULL,
   v integer NOT NULL DEFAULT 0
 );
-
-
-
 
 
 -- for some test queries as user megxuser
 -- SET ROLE megxuser
 
 
-rollback;
-
-
+commit;
