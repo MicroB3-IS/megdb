f84032c73ce2cfa4c2f3a74b27bac7e730ed0997
renzo <renzok@users.noreply.github.com>
2014-11-27 09:26:00 +0000

    aded missing schema qualifier

--- patches/00100-mibig-seq-perms-patch.sql
+++ patches/00100-mibig-seq-perms-patch.sql
@@ -6,8 +6,8 @@ SELECT _v.register_patch('00100-mibig-seq-perms',
 -- section of creation best as user role megdb_admin
 SET ROLE megdb_admin;
 
-GRANT USAGE ON SEQUENCE gene_submissions_id_seq TO megxuser;
-GRANT USAGE ON SEQUENCE nrps_submissions_id_seq TO megxuser;
+GRANT USAGE ON SEQUENCE mibig.gene_submissions_id_seq TO megxuser;
+GRANT USAGE ON SEQUENCE mibig.nrps_submissions_id_seq TO megxuser;
 
 
 -- for some test queries as user megxuser
