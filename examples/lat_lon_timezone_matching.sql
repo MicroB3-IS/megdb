050b95a231349e6feed53364b540fc990eab5a60
renzo <renzok@users.noreply.github.com>
2015-03-04 18:55:32 +0000

    example of how to find out the timezone to a lat/lon

---
+++ examples/lat_lon_timezone_matching.sql
@@ -0,0 +1,10 @@
+select osd_id,label,label_verb,start_lat,start_lon, scalerank, featurecla, "name", map_color6, map_color8, 
+       note, "zone", utc_format, time_zone, iso_8601, places, dst_places, 
+       tz_name1st, tz_namesum
+
+  from
+    osdregistry.samples 
+  left join 
+    tz_world.world_time_zones tz
+  ON ( ST_intersects(samples.start_geom,  tz.geom) )
+  where  scalerank is not null;
