0bc34aea4ab47c232c9814004c2b791c5488b6d0
renzo <renzok@users.noreply.github.com>
2015-03-08 19:25:20 +0000

    nice way of getting to know a timezone

--- examples/lat_lon_timezone_matching.sql
+++ examples/lat_lon_timezone_matching.sql
@@ -1,3 +1,5 @@
+
+
 select osd_id,label,label_verb,start_lat,start_lon, scalerank, featurecla, "name", map_color6, map_color8, 
        note, "zone", utc_format, time_zone, iso_8601, places, dst_places, 
        tz_name1st, tz_namesum
